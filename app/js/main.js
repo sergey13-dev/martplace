bf9d8b1ac19a475dd048c4d56b84f0ba063391d3
sergey13-dev <olkarsergeynew@gmail.com>
2020-04-07 09:19:32 +0300

    added slider featured

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function () {
 
+    $(".slider__resume").slick({
+        appendArrows: $(".featured__weekly-button"),
+
+        prevArrow: '<button class="slick-arrow slick-prev"> <img src="images/slider_resume/slider-left.png" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"> <img src="images/slider_resume/slider-right.png" alt=""></button>',
+
+    });
+
+
+
+
+
+
     //var mixer = mixitup('');
 
 
