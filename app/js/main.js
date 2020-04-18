0206cec56c012065fd75bf23f1c2db698d29ab08
sergey13-dev <olkarsergeynew@gmail.com>
2020-04-18 07:43:39 +0300

    added all

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,86 @@
 $(function () {
 
     $(".slider__resume").slick({
-        appendArrows: $(".featured__weekly-button"),
+        appendArrows: $(".featured__button"),
 
-        prevArrow: '<button class="slick-arrow slick-prev"> <img src="images/slider_resume/slider-left.png" alt=""></button>',
-        nextArrow: '<button class="slick-arrow slick-next"> <img src="images/slider_resume/slider-right.png" alt=""></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"> <img src="images/slider_resume/chevron-left.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"> <img src="images/slider_resume/chevron-right.svg" alt=""></button>',
 
     });
 
 
-    var mixer = mixitup('.products__inner-box');
 
+    $(".slider__bottom").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        appendArrows: $(".followers__feed-button"),
+
+        prevArrow: '<button class="slick-arrow slick-prev"> <img src="images/slider_resume/chevron-left.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"> <img src="images/slider_resume/chevron-right.svg" alt=""></button>',
+
+    });
+
+
+    // рейтинг
+    $(".rate-star").rateYo({
+
+        rating: 4.5,
+        starWidth: "12px",
+        readOnly: true,
+
+    });
+
+    //<div class="rate-star"></div>
+
+
+
+    $(".menu__btn").on("click", function () {
+        $(".menu__list").slideToggle();
+    });
+
+
+
+
+
+
+
+
+
+
+
+    // search__products-form-one
+    //js-example-basic-single
+    $(".js-example-basic-single").select2();
+
+
+
+
+    // для всех select
+    $('select').styler();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // фильтер, if чтобы небыло ошибки 
+    // для фильтра БЛОКА products всегда указываем внизу 
+    if ($('.products__inner-box').length) {
+        let mixer = mixitup('.products__inner-box');
+    }
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,11 @@ gulp.task("script", function () {
     return gulp.src([
         "node_modules/slick-carousel/slick/slick.js",
         "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
-        "node_modules/mixitup/dist/mixitup.js"
+        "node_modules/mixitup/dist/mixitup.js",
+        "node_modules/rateyo/src/jquery.rateyo.js",
+        "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
+        "node_modules/Select2/select2.js",
+        "node_modules/jquery-form-styler/dist/jquery.formstyler.js",
     ])
         .pipe(concat("libs.min.js"))
         .pipe(uglify())
@@ -72,7 +76,15 @@ gulp.task("style", function () {
         "node_modules/normalize.css/normalize.css",
         "node_modules/slick-carousel/slick/slick-theme.css",
         "node_modules/slick-carousel/slick/slick.css",
-        "node_modules/magnific-popup/dist/magnific-popup.css"
+        "node_modules/magnific-popup/dist/magnific-popup.css",
+        "node_modules/rateyo/src/jquery.rateyo.css",
+        "node_modules/Select2/select2.css",
+        // для регулятора цен 
+        "node_modules/ion-rangeslider/css/ion.rangeSlider.css",
+
+        "node_modules/jquery-form-styler/dist/jquery.formstyler.css",
+        "node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css"
+
         // "node_modules/animate.css/animate.css"
 
     ])
